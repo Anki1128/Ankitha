3353f7ba832632f38c36931b1593783351720993
desin <desineniankitha@gmail.com>
2023-09-28 17:32:29 -0400

    commit

--- src/main/java/com/example/postgresqldatabase/controller/DepartmentController.java
+++ src/main/java/com/example/postgresqldatabase/controller/DepartmentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/departments")
+@RequestMapping("/department")
 public class DepartmentController {
     private final DepartmentService departmentService;
 
